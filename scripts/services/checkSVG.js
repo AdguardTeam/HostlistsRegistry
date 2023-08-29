dcf3e3ca3473348baa36b4b0fe1f633cfc74195e
Elizaveta Egorova <e.egorova@adguard.com>
2023-08-29 16:13:33 +0300

    added throwing errors

--- scripts/services/checkSVG.js
+++ scripts/services/checkSVG.js
@@ -2,50 +2,40 @@ const { DOMParser } = require('xmldom')
 
 // get SVG string and parse it to SVG tag
 const checkSVG = (serviceName, SVGString) => {
-    // check escape quotes
-    const isQuotesEscaped = (SVGString) => {
-        return /\"/.test(SVGString)
-    }
-    if(!isQuotesEscaped(SVGString)){
-        console.log(serviceName + ' : Double quotes inside the SVG tag must be escaped so as not to break JSON')
-        return false;
-    }
-    // remove escape characters from svg string
-    const getSVGTag = (SVGString) => {return SVGString.replace(/\\/, '')}
-    const SVGTag = getSVGTag(SVGString);
-    // parse SVG in DOM Document and check syntax
-    const parseSVG = (SVGTag) => {
-        const parser = new DOMParser({locator: {},errorHandler:{error: serviceName + ' :SVG syntax error'}});
+    // parse SVG in DOM document and check syntax
+    const parseSVG = (SVGString) => {
+        const parser = new DOMParser({locator: {},errorHandler:{}});
         try {
-            const SVGObject = parser.parseFromString(SVGTag);
-            return SVGObject
+            const SVGObject = parser.parseFromString(SVGString);
+            if(!SVGObject){
+                throw new Error(serviceName + ' : SVG syntax error');
+            }
+            return SVGObject;
         } catch (error) {
-            console.log(parser.options.errorHandler.error);
-            return null
+            throw error
         }
     }
-    const svgNode = parseSVG(SVGTag);
-    // returns if syntax error in svg
-    if (!svgNode) {
-        return false;
+    try {
+        const svgNode = parseSVG(SVGString, serviceName);
+        if (!svgNode.childNodes[0].nodeName || svgNode.childNodes[0].nodeName !== 'svg') {
+            throw new Error(serviceName + ' : Parsed SVG object is undefined');
+        }
+        const svgElement = svgNode.documentElement;
+        // check width and height tags
+        if(svgElement.hasAttribute('height') || svgElement.hasAttribute('width')){
+            throw new Error(serviceName + ' : Svg tag must not contain `width` and `height` attributes')
+        }
+        // check fill='currentColor'
+        if((!svgElement.hasAttribute('fill')) || (svgElement.getAttribute('fill') !== 'currentColor')){
+            throw new Error(serviceName + ' : Svg tag must contain `fill="currentColor"` attribute. This is important for themes to work properly.')
+        }
+        // check if svg is square
+        const svgViewBox = svgElement.getAttribute('viewBox').split(' ')
+        if(svgViewBox[2] !== svgViewBox[3]){
+            throw new Error(serviceName + ' : The icon must have a square shape, i.e. the third and fourth parameters of the `viewBox` attribute must be equal.')
+        }
+    } catch (error) {
+        throw error
     }
-    const svgElement = svgNode.documentElement
-    // check width and height tags
-    if(svgElement.hasAttribute('height') || svgElement.hasAttribute('width')){
-        console.log(serviceName + ' : Svg tag must not contain `width` and `height` attributes')
-        return false;
-    }
-    // check fill='currentColor'
-    if((!svgElement.hasAttribute('fill')) || (svgElement.getAttribute('fill') !== 'currentColor')){
-        console.log(serviceName + ' : Svg tag must contain `fill="currentColor"` attribute. This is important for themes to work properly.')
-        return false;
-    }
-    // check if svg is square
-    const svgViewBox = svgElement.getAttribute('viewBox').split(' ')
-    if(svgViewBox[2] !== svgViewBox[3]){
-        console.log(serviceName + ' : The icon must have a square shape, i.e. the third and fourth parameters of the `viewBox` attribute must be equal.')
-        return false;
-    }
-    return true;
 }
 module.exports = {checkSVG}
