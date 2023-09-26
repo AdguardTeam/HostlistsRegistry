17411a898d5dbb8e5ba1eb497cd8f20a0615e358
jellizaveta <91964807+jellizaveta@users.noreply.github.com>
2023-09-26 17:37:14 +0300

    Update scripts/services/rewrite-services-json.js
    Co-authored-by: Slava <45171506+slavaleleka@users.noreply.github.com>

--- scripts/services/rewrite-services-json.js
+++ scripts/services/rewrite-services-json.js
@@ -8,7 +8,6 @@ const { checkSVG } = require('./check-svg');
  * Checks that all services have valid SVG icons.
  *
  * @param {Array<object>} servicesArray - An array of service data objects.
- * @returns {boolean} True if all services have valid SVG icons.
  * @throws {Error} If an error(s) occurred during SVG validation, an error is thrown.
  */
 const validateSvgIcons = (servicesArray) => {
