8015d7279fc550533c3469b0af9baec3e333fedd
Elizaveta Egorova <e.egorova@adguard.com>
2023-11-30 01:19:00 +0300

    localization development

--- scripts/services/add-localizations.js
+++ scripts/services/add-localizations.js
@@ -1,43 +1,86 @@
-const fs = require('fs');
+const { promises: fs } = require('fs');
 const path = require('path');
 
-const localesDirPath = path.join(__dirname, '../../locales');
+const LOCALES_DIRECTORY_PATH = path.join(__dirname, '../../locales');
 
-const servicesFile = 'services.json';
-const i18_file = path.join(__dirname, '../../assets/services_i18n.json');
+const SERVICES_FILE_NAME = 'services.json';
+const I18_FILE_PATH = path.join(__dirname, '../../assets/services_i18n.json');
 
-const getLocales = async (services, i18) => {
-    // Получаем полный путь до директории
-    const getDirFilePaths = (folderPath) => fs.readdirSync(folderPath)
-        .filter((file) => fs.statSync(path.join(folderPath, file)).isDirectory())
-        .map((file) => path.join(folderPath, file));
+/**
+ * Returns only directories names from folder
+ *
+ * @param {string} folderPath folder path
+ * @returns {Array<string>} only directories names in folder
+ */
+const getDirFileNames = async (folderPath) => (await fs.readdir(folderPath, { withFileTypes: true }))
+    .filter((folderFile) => folderFile.isDirectory())
+    .map((folderFile) => folderFile.name);
 
-    const dirFilePaths = getDirFilePaths(services);
+/**
+ *
+ * @param {string} folderPath //
+ * @param {string} targetFile //
+ * @returns
+ */
+const findDirectoriesWithFile = async (folderPath, targetFile) => {
+    const dirNames = await getDirFileNames(folderPath);
+    const results = await Promise.all(
+        dirNames.map(async (dirName) => {
+            const dirPath = path.join(folderPath, dirName, targetFile);
+            const isExist = async (filePath) => fs.access(filePath, fs.constants.F_OK)
+                .then(() => true)
+                .catch(() => false);
+            if (await isExist(dirPath)) {
+                return dirName;
+            }
+            return null;
+        }),
+    );
 
-    const localeObject = {};
-    const ValueObject = {};
-
-    const result = [];
-
-    dirFilePaths.map((dirFilePath) => {
-        if (fs.existsSync(path.join(dirFilePath, servicesFile))) {
-            const folder = dirFilePath.split('/').pop();
-            const folderContent = fs.readFileSync(path.join(dirFilePath, servicesFile));
-            const contentChunk = JSON.parse(folderContent);
-            contentChunk.map((groupChunk) => {
-                const group = Object.entries(groupChunk);
-                for (let i = 0; i < group.length; i += 1) {
-                    const combinedGroup = group[i];
-                    const [prefix, id, postfix] = combinedGroup[0].split('.');
-                    ValueObject[postfix] = combinedGroup[1];
-                    localeObject[folder] = ValueObject;
-                    result[id] = localeObject;
-                }
-            });
-        }
-    });
-
-    fs.writeFileSync(i18, JSON.stringify(result));
+    return results.filter((result) => result !== null);
 };
 
-getLocales(localesDirPath, i18_file);
+const groupLocaleObject = (fileObjects, locale) => {
+    const categoryObjects = {};
+    fileObjects.forEach((fileObject) => {
+        Object.entries(fileObject).forEach(([key, value]) => {
+            const [category, id, sign] = key.split('.');
+            const translate = { [sign]: value };
+            const localesTranslate = { [locale]: translate };
+            const componentLocalesTranslate = { [id]: localesTranslate };
+            if (categoryObjects[category]) {
+                // Category already exists, add to it
+                Object.assign(categoryObjects[category], componentLocalesTranslate);
+            } else {
+                // Category doesn't exist, create a new one
+                categoryObjects[category] = componentLocalesTranslate;
+            }
+        });
+    });
+
+    return categoryObjects;
+};
+
+const getLocaleObject = async (folder, directories, file) => {
+    const promises = directories.map(async (directory) => {
+        const filePath = path.join(folder, directory, file);
+        const fileContent = JSON.parse(await fs.readFile(filePath));
+        return groupLocaleObject(fileContent, directory);
+    });
+    return Promise.all(promises);
+};
+
+const getLocales = async (localesFolderPath, serviceFile) => {
+    const directoriesWithFile = await findDirectoriesWithFile(localesFolderPath, serviceFile);
+    const categoryLocalesTranslate = await getLocaleObject(localesFolderPath, directoriesWithFile, serviceFile);
+    return Object.assign({}, ...categoryLocalesTranslate);
+};
+
+const writeI18File = async (localesFolderPath, serviceFile, i18FilePath) => {
+    const localizations = await getLocales(localesFolderPath, serviceFile);
+    await fs.writeFile(i18FilePath, JSON.stringify(localizations, null, 4));
+};
+
+(async () => {
+    await writeI18File(LOCALES_DIRECTORY_PATH, SERVICES_FILE_NAME, I18_FILE_PATH);
+})();
