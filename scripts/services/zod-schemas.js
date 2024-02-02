0756377f0be0a9b95be3bad662cda6434c850208
Elizaveta Egorova <e.egorova@adguard.com>
2024-02-02 17:40:12 +0300

    added comments and examples for validation schemes

--- scripts/services/zod-schemas.js
+++ scripts/services/zod-schemas.js
@@ -1,6 +1,13 @@
 const { z } = require('zod');
 
-// { 'servicesgroup.cdn.name': 'Content Delivery Network' }
+/**
+ * Zod schema for translating a specific service group.
+ * The key should start with 'servicesgroup.' and end with '.name' and the value should be a string.
+ * Should be only one property in the object.
+ *
+ * @typedef {{ [key: string]: string }} TranslationSchema
+ * @example { 'servicesgroup.cdn.name': 'Content Delivery Network' }
+ */
 const translationSchema = z.record(
     z.string().regex(/^servicesgroup\.[a-z_]+\.name$/),
     z.string(),
@@ -8,28 +15,64 @@ const translationSchema = z.record(
     message: 'Object must have exactly 1 property',
 });
 
-// [{ 'servicesgroup.cdn.name': 'Content Delivery Network' }]
+/**
+ * Zod schema for an array of translation objects.
+ *
+ * @typedef {TranslationSchema[]} TranslationsCollectionSchema
+ * @example [{ 'servicesgroup.cdn.name': 'Content Delivery Network' }]
+ */
 const translationsCollectionSchema = z.array(translationSchema);
 
-// {'name': 'Content Delivery Network'}
+/**
+ * Zod schema for translation of specific name in a locale.
+ *
+ * @typedef {{ name: string }} TranslationNameSchema
+ * @example {'name': 'Content Delivery Network'}
+ */
 const translationNameSchema = z.object({ name: z.string() }).strict();
 
-// {'en': {'name': 'Content Delivery Network'}}
+/**
+ * Zod schema for translating a specific service group in different locales.
+ *
+ * @typedef {{ [key: string]: TranslationNameSchema }} TranslationLocaleSchema
+ * @example {'en': {'name': 'Content Delivery Network'}}
+ */
 const translationLocaleSchema = z.record(
     z.string().regex(/^[a-zA-Z_]+$/),
     translationNameSchema,
 );
 
-// {'cdn':{'en': {'name': 'Content Delivery Network'}}}
+/**
+ * Zod schema for translating a specific service group in different locales.
+ *
+ * @typedef {{ [key: string]: TranslationLocaleSchema }} TranslationIdSchema
+ * @example {'cdn':{'en': {'name': 'Content Delivery Network'}}}
+ */
 const translationIdSchema = z.record(
     z.string().regex(/^[a-z_]+$/),
     translationLocaleSchema,
 );
 
-// {'groups': {'cdn':{'en': {'name': 'Content Delivery Network'}}}}
+/**
+ * Zod schema for a set of translation data for service groups.
+ *
+ * @typedef {{ groups: TranslationIdSchema }} ServicesI18Schema
+ * @example {'groups': {'cdn':{'en': {'name': 'Content Delivery Network'}}}}
+ */
 const servicesI18Schema = z.object({ groups: translationIdSchema });
 
-// { id: 'cdn', name: 'Content Delivery Network', rules: [], icon_svg: '', group: 'cdn' }
+/**
+ * Zod schema for a service.
+ *
+ * @typedef {{
+ *   id: string;
+ *   name: string;
+ *   rules: string[];
+ *   icon_svg: string;
+ *   group: string;
+ * }} ServiceSchema
+ * @example { id: 'cdn', name: 'Content Delivery Network', rules: [], icon_svg: '', group: 'cdn' }
+ */
 const serviceSchema = z.object({
     id: z.string(),
     name: z.string(),
@@ -38,7 +81,15 @@ const serviceSchema = z.object({
     group: z.string(),
 }).strict();
 
-// { blocked_services: [], groups: [{ id: 'cdn' }] }
+/**
+ * Zod schema for services grouped by blocked_services and groups.
+ *
+ * @typedef {{
+ *   blocked_services: ServiceSchema[];
+ *   groups: { id: string }[];
+ * }} GroupedServicesSchema
+ * @example { blocked_services: [], groups: [{ id: 'cdn' }] }
+ */
 const groupedServicesSchema = z.object({
     blocked_services: z.array(serviceSchema),
     groups: z.array(z.object({ id: z.string() }).strict()),
