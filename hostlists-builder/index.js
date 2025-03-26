8a4eda172de8f4e9bd88bc11afc5724adcb212c1
Andrey Meshkov <am@adguard.com>
2025-03-26 19:08:29 +0300

    improve error logging

--- hostlists-builder/index.js
+++ hostlists-builder/index.js
@@ -208,7 +208,7 @@ async function build(filtersDir, tagsDir, localesDir, assetsDir, groupsDir) {
           });
         }
       } catch (ex) {
-        throw new Error(`Failed to compile ${metadata.filterId}: ${ex}`);
+        throw new Error(`Failed to compile ${metadata.filterId}:`, ex);
       }
     }
 
